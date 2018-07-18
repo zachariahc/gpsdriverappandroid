e60fbba5c4cf46226b8272f433946aa3e38f5f20
Zachariah Crowell <zachariahc@Zachariahs-MacBook-Air.local>
2018-07-17 20:57:45 -0600

    more files

---
+++ app/src/main/java/com/example/gpsdriver/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.nomeateats;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    WebView webview;
+
+
+    @Override
+    public void onBackPressed() {
+        if (webview.canGoBack()){
+            webview.goBack();
+        } else {
+
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        webview = (WebView) findViewById(R.id.webview);
+
+        WebSettings webSettings = webview.getSettings();
+
+        webSettings.setJavaScriptEnabled(true);
+
+        webview.setWebViewClient(new WebViewClient());
+
+        webview.loadUrl("http://www.zachariahcrowell.com/weeklyschedule.html");
+
+    }
+}
